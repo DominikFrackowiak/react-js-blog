b3acbc09a3d68bf6757937902ed4b01c7e5f6ba5
DominikFrackowiak <rorschach@op.pl>
2023-01-03 13:02:48 +0100

    beta version of the blog finished

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+const year = new Date().getFullYear()
+
+const Footer = () => {
+	return (
+		<footer className='Footer'>
+			<p>Copyright &copy; {year}</p>
+		</footer>
+	)
+}
+
+export default Footer
---
+++ src/NewPost.jsx
@@ -0,0 +1,33 @@
+const NewPost = ({
+	handleSubmit,
+	postTitle,
+	setPostTitle,
+	postBody,
+	setPostBody,
+}) => {
+	return (
+		<main className='NewPost'>
+			<h1>NewPost</h1>
+			<form className='newPostForm' onSubmit={handleSubmit}>
+				<label htmlFor='postTitle'>Title:</label>
+				<input
+					type='text'
+					id='postTitle'
+					value={postTitle}
+					onChange={e => setPostTitle(e.target.value)}
+					required
+				/>
+				<label htmlFor='postBody'>Post:</label>
+				<textarea
+					id='postBody'
+					value={postBody}
+					onChange={e => setPostBody(e.target.value)}
+					required
+				/>
+				<button type='submit'>Submit</button>
+			</form>
+		</main>
+	)
+}
+
+export default NewPost
